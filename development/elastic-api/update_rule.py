317b81eb5b55f226713bbd73bc010b275e2baff7
nvb <n.vanberendonk@gmail.com>
2024-05-29 15:48:18 +0200

    improved update script

--- development/elastic-api/update_rule.py
+++ development/elastic-api/update_rule.py
@@ -14,21 +14,25 @@ headers = {
     'Authorization': 'ApiKey ' + api_key
 }
 
+changed_files = os.environ['CHANGED_FILES']
+
+required_fields = [] 
 # use custom_test_alerts
 #for root, dirs, files in os.walk("detections/"): #set path
 for root, dirs, files in os.walk("detections/"): #set path -> folder for testing single rule
     for file in files:
-        print(file)
-        data = "{\n"
-        if file.endswith(".toml"):
-            full_path = os.path.join(root, file)
+        if file in changed_files:
+            print(file)
+            data = "{\n"
+            if file.endswith(".toml"):
+                full_path = os.path.join(root, file)
             
-            with open(full_path,"rb") as toml:
-                alert = tomllib.load(toml)
-                print(str(alert['rule']['name']))
+                with open(full_path,"rb") as toml:
+                    alert = tomllib.load(toml)
+                    print(str(alert['rule']['name']))
 
                 #set required fields:
-                required_fields = []              
+                             
                 if alert['rule']['type'] == "query": # query based aler
                     required_fields = ['author', 'description', 'name', 'rule_id', 'risk_score', 'type', 'severity', 'query', 'threat']
                 elif alert['rule']['type']== "eql": # event correlation alert
@@ -45,21 +49,32 @@ for root, dirs, files in os.walk("detections/"): #set path -> folder for testing
                         if type(alert['rule'][field]) == list:
                             data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + ",\n"
                         elif type(alert['rule'][field]) == str:
-                            
-                            if field == "query":
-                                data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\\","\\\\").replace("\"", "\\\"").replace("\n", " ") + "\"," + "\n"
-
+                            # if field == "query":
+                            #     data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\\","\\\\").replace("\"", "\\\"").replace("\n", " ") + "\"," + "\n"
+                            # else:
+                            #     data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"", "\\\"").replace("\\", "\\\\") + "\"," + "\n"
+                            if field == 'description':
+                                data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"","\\\"").replace("\\","\\\\") + "\"," + "\n"
+                            elif field == 'query':
+                                data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\\","\\\\").replace("\"","\\\"").replace("\n"," ") + "\"," + "\n"
                             else:
-                                data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"", "\\\"").replace("\\", "\\\\") + "\"," + "\n"
+                                data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"","\\\"") + "\"," + "\n"
                         elif type(alert['rule'][field]) == int:
-                            data += "  " + "\"" + field + "\": " + str(alert['rule'][field]) + ",\n"                    
+                            data += "  " + "\"" + field + "\": " + str(alert['rule'][field]) + ",\n"
+                        elif type(alert['rule'][field]) == dict:
+                            data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + "," + "\n"                
                 data += "  \"enabled\": true\n}"
         
         #print(data)
-        rule_id = [alert]["rule"]["rule_id"]
-        url = url + "?rule_id=" + rule_id
+            rule_id = [alert]["rule"]["rule_id"]
+            url = url + "?rule_id=" + rule_id
 
-        elastic_data = requests.put(url, headers=headers, data=data).json()
+            elastic_data = requests.put(url, headers=headers, data=data).json()
+            for key in elastic_data:
+                if key == "status_code":
+                    if 404 == elastic_data["status_code"]:
+                        elastic_data = requests.post(url, headers=headers, data=data).json()
+                        print(elastic_data)
 
-        print(elastic_data)
-        print("##########")
+            #print(elastic_data)
+            #print("##########")
